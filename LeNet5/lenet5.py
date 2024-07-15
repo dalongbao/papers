301f3bf4f9536a960bb43786be1e788abd928fce
tsuiusi <ryantsui786@gmail.com>
2024-07-15 19:46:19 +0800

    training loop for lenet

--- LeNet5/lenet5.py
+++ LeNet5/lenet5.py
@@ -1,8 +1,10 @@
 import mlx
 import mlx.nn as nn
 import mlx.core as mx
+import mlx.optimizers as optim
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 """
 Structure:
@@ -57,7 +59,55 @@ class LeNet(nn.Module):
         
         return x
 
-model = LeNet()
+def loss_fn(model, X, y):
+    return mx.mean(nn.losses.cross_entropy(model(X), y))
 
-lr = 1e-1
+def eval_fn(model, X, y):
+    return mx.mean(mx.argmax(model(X), axis=1) == y)
+
+def batch_iterate(batch_size, X, y):
+    perm = mx.array(np.random.permutation(y.size))
+    for i in range(0, y.size, batch_size):
+        ids = perm[i: i + batch_size]
+        yield X[ids], y[ids]
+
+def predict(model, image):
+    image = mx.array(image)
+    image = image.reshape(-1, 1)
+    prediction = model(image)
+    predicted_class = mx.argmax(prediction, axis=1)
+    return predicted_class.item()
+
+def main():
+    model = LeNet()
+    mx.eval(model.parameters())
+
+    lr = 1e-1
+    num_epochs = 10
+
+    # Load data here as train_images, train_data, val_images, val_data
+
+    loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+    optimizer = optim.AdamW(learning_rate=lr)
+
+    for i in range(num_epochs):
+        tic = time.perf_counter()
+        for X, y in batch_iterate(256, train_images, train_data):
+            loss, grad = loss_and_grad_fn(model, X, y)
+            optimizer.update(model, grads)
+            mx.eval(model.parameters(), optimizer.state)
+
+        accuracy = eval_fn(model, test_images, test_labels)
+        toc = time.perf_counter()
+
+        print(f'Epoch {e} | Accuracy: {accuracy.item():.3f} | Time: {(toc - tic):.3f}s')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('Train a simple MLP on MNIST')
+    parser.add_argument('--gpu', action='store_true', help='Use the Metal backend')
+    args = parser.parse_args()
+    if not args.gpu:
+        mx.set_default_device(mx.cpu)
+    main()
+
 
